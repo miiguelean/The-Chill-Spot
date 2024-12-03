0b510cbab16077964ce9c3cfe61769c6fb7793da
Miguel Tienda <112979151+miiguelean@users.noreply.github.com>
2024-12-03 17:35:53 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 if (!self.define) {
-  let e,
-    i = {};
+  let e, i = {};
   const r = (r, s) => (
     (r = new URL(r + ".js", s).href),
     i[r] ||
@@ -29,46 +28,77 @@ if (!self.define) {
 }
 define(["./workbox-48867127"], function (e) {
   "use strict";
+  // Firebase configuration
+  importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js');
+  importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js');
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCLATOoJaHm1ik6fCWHJYAkPgIhMQ5B5O4",
+    authDomain: "the-chill-spot-b94ae.firebaseapp.com",
+    projectId: "the-chill-spot-b94ae",
+    storageBucket: "the-chill-spot-b94ae.firebasestorage.app",
+    messagingSenderId: "519997067329",
+    appId: "1:519997067329:web:023cffe155db50c797f6c0",
+    measurementId: "G-NS2XWVYFF2"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const messaging = firebase.messaging();
+
   self.addEventListener("message", (e) => {
     e.data && "SKIP_WAITING" === e.data.type && self.skipWaiting();
-  }),
-    e.precacheAndRoute(
-      [
-        {
-          url: "css/estilos.css",
-          revision: "7e7c656ab7b373e8c8152969909383a8",
-        },
-        { url: "img/1.jpg", revision: "8120a40180dba833fe0b75abe7bfbfe5" },
-        { url: "img/2.jpg", revision: "b20d2557fa8627db45e3ba3d2e2f1372" },
-        { url: "img/3.jpg", revision: "a5d05f6149d5866bbd9b7b8cb6c70dc5" },
-        { url: "img/4.jpg", revision: "8e0249cb5a0613f217a9fc22f3f0f690" },
-        { url: "img/5.jpg", revision: "8b61b001e77d2e44a3f54a7ed00c7ae5" },
-        { url: "img/6.jpg", revision: "4a9d2554f9a7340ff23cd02148ec91f0" },
-        { url: "img/7.jpg", revision: "01e216ed6920846c096c7e78462d295a" },
-        { url: "img/8.jpg", revision: "edb2fbabb00a555c1558de8427961968" },
-        {
-          url: "img/bg-textura.png",
-          revision: "fa6a95e71d181ae6ac1f7586f0d8515a",
-        },
-        { url: "img/bg.jpg", revision: "9ab5941cd0b719b26b14b41e1f107f6f" },
-        {
-          url: "img/icon-192x192.jpg",
-          revision: "ff2efe01ba360ae47f453f13d588f29b",
-        },
-        {
-          url: "img/icon-512x512.jpg",
-          revision: "30e31f169d02c62c5b0d7049c153f527",
-        },
-        { url: "img/icon.ico", revision: "12244b97427b53ffef697987ac3ff6a4" },
-        { url: "index.html", revision: "31e89878aa143ab9f2b83f0585373d03" },
-        { url: "js/efectos.js", revision: "195611958bb7978ac5e92731dd01e0fe" },
-        {
-          url: "js/jquery-1.12.3.min.js",
-          revision: "2b6294333db8eeb65bc7717144357d23",
-        },
-        { url: "js/parallax.js", revision: "9d1a4e508c779a2087099b439b6d1a06" },
-        { url: "manifest.json", revision: "641500040809ddcd2210008046527663" },
-      ],
-      { ignoreURLParametersMatching: [/^utm_/, /^fbclid$/] }
-    );
+  });
+
+  e.precacheAndRoute(
+    [
+      {
+        url: "css/estilos.css",
+        revision: "7e7c656ab7b373e8c8152969909383a8",
+      },
+      { url: "img/1.jpg", revision: "8120a40180dba833fe0b75abe7bfbfe5" },
+      { url: "img/2.jpg", revision: "b20d2557fa8627db45e3ba3d2e2f1372" },
+      { url: "img/3.jpg", revision: "a5d05f6149d5866bbd9b7b8cb6c70dc5" },
+      { url: "img/4.jpg", revision: "8e0249cb5a0613f217a9fc22f3f0f690" },
+      { url: "img/5.jpg", revision: "8b61b001e77d2e44a3f54a7ed00c7ae5" },
+      { url: "img/6.jpg", revision: "4a9d2554f9a7340ff23cd02148ec91f0" },
+      { url: "img/7.jpg", revision: "01e216ed6920846c096c7e78462d295a" },
+      { url: "img/8.jpg", revision: "edb2fbabb00a555c1558de8427961968" },
+      {
+        url: "img/bg-textura.png",
+        revision: "fa6a95e71d181ae6ac1f7586f0d8515a",
+      },
+      { url: "img/bg.jpg", revision: "9ab5941cd0b719b26b14b41e1f107f6f" },
+      {
+        url: "img/icon-192x192.jpg",
+        revision: "ff2efe01ba360ae47f453f13d588f29b",
+      },
+      {
+        url: "img/icon-512x512.jpg",
+        revision: "30e31f169d02c62c5b0d7049c153f527",
+      },
+      { url: "img/icon.ico", revision: "12244b97427b53ffef697987ac3ff6a4" },
+      { url: "index.html", revision: "31e89878aa143ab9f2b83f0585373d03" },
+      { url: "js/efectos.js", revision: "195611958bb7978ac5e92731dd01e0fe" },
+      {
+        url: "js/jquery-1.12.3.min.js",
+        revision: "2b6294333db8eeb65bc7717144357d23",
+      },
+      { url: "js/parallax.js", revision: "9d1a4e508c779a2087099b439b6d1a06" },
+      { url: "manifest.json", revision: "641500040809ddcd2210008046527663" },
+    ],
+    { ignoreURLParametersMatching: [/^utm_/, /^fbclid$/] }
+  );
+  
+  messaging.onBackgroundMessage(function(payload) {
+    console.log('Recibido mensaje en segundo plano:', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: payload.notification.icon,
+      badge: payload.notification.badge
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
 });
