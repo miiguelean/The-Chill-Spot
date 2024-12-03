abdd41e74bfd57f01181d3c6e2304ab80c04fff8
miigulean <mitwi26@gmail.com>
2024-12-03 04:26:33 -0600

    firebase

--- firebase-init.js
+++ firebase-init.js
@@ -23,7 +23,7 @@ const messaging = getMessaging(app);
 
 // Registrar tu Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('firebase-messaging-sw.js')
+  navigator.serviceWorker.register('/The-Chill-Spot/firebase-messaging-sw.js')
     .then((registration) => {
       console.log('Service Worker registrado con éxito:', registration);
 
